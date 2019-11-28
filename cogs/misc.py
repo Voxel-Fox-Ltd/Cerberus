d3729fbd8440fbfe3e38e7a66a9de8f215c02de0
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-11-28 00:52:54 +0000

    Fix up the misc cog

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,21 +1,17 @@
-import discord
-from discord.ext.commands import commands
+from discord.ext import commands
 
 from cogs import utils
 
+
 class Misc(utils.cog):
 
-  def __init__(self, bot:utils.CustomBot):
-    super().__init__(bot)
-    
-  @commands.command()
-  async def github(self, ctx:utils.Context):
-      '''
-      Sends the GitHub Repository link
-      '''
+    @commands.command()
+    async def github(self, ctx:utils.Context):
+        """Sends the GitHub Repository link"""
 
-      await ctx.send("<https://github.com/4Kaylum/Cerberus/>")
-      
-  def setup(bot:utils.CustomBot):
+        await ctx.send("<https://github.com/4Kaylum/Cerberus/>")
+
+
+def setup(bot:utils.CustomBot):
     x = Misc(bot)
     bot.add_cog(x)
