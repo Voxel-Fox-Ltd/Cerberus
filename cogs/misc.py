20058d7502a04f41ff66ac30e63a4e6d395300db
iHeroGH <32426140+iHeroGH@users.noreply.github.com>
2019-11-27 16:39:25 -0800

    Created a Misc cog
    and added a GitHub command

---
+++ cogs/misc.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext.commands import commands
+
+from cogs import utils
+
+class Misc(utils.cog):
+
+  def __init__(self, bot:utils.CustomBot):
+    super().__init__(bot)
+    
+  @commands.command()
+  async def github(self, ctx:utils.Context):
+      '''
+      Sends the GitHub Repository link
+      '''
+
+      await ctx.send("<https://github.com/4Kaylum/Cerberus/>")
