f2dc21cc6129746ab25ade6746cf867336abbd0b
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-12-07 23:21:37 +0000

    Add server and invite commands

--- cogs/misc.py
+++ cogs/misc.py
@@ -11,6 +11,18 @@ class Misc(utils.Cog):
 
         await ctx.send(f"<{self.bot.config.get('github')}>")
 
+    @commands.command(aliases=['support', 'guild'])
+    async def server(self, ctx:utils.Context):
+        """Gives the invite to the support server"""
+
+        await ctx.send(f"<{self.bot.config.get('guild_invite')}>")
+
+    @commands.command()
+    async def invite(self, ctx:utils.Context):
+        """Gives you the bot's invite link"""
+
+        await ctx.send(f"<{self.bot.get_invite_link()}>")
+
 
 def setup(bot:utils.CustomBot):
     x = Misc(bot)
