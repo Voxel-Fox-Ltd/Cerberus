dbe3cc62c022282dc3f7a1e5b0c29d1102c5c144
catdotjs <catdotjs@gmail.com>
2023-10-16 20:04:07 +0300

    added main.go and other mod packages

---
+++ main.go
@@ -0,0 +1,11 @@
+package main;
+
+import (
+  "fmt"
+  // "github.com/bwmarrin/discordgo"
+)
+
+func main(){
+  fmt.Println("Cerby will be here soon <3");
+  // discord, err := discordgo.New("Bot [TOKEN HERE]");
+}
